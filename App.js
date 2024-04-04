ae96f164768d64cbdcb178cebeec47b95eaa800e
halilxibrahim <ibrahim_kamaci_06@hotmail.com>
2024-04-04 14:41:57 +0300

    build text-to-image ai app first

--- App.js
+++ App.js
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, Image } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [userInput, setUserInput] = useState('');
+  const [imageUrl, setImageUrl] = useState(null);
+
+  const handleInputChange = (text) => {
+    setUserInput(text);
+  };
+
+  const generateImage = async () => {
+    const data = { inputs: userInput };
+    const response = await fetch(
+      "https://api-inference.huggingface.co/models/SG161222/Realistic_Vision_V1.4",
+      {
+        headers: { Authorization: "Bearer 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" },
+        method: "POST",
+        body: JSON.stringify(data),
+      }
+    );
+
+    if (response.ok) {
+      const blob = await response.blob();
+      const url = URL.createObjectURL(blob);
+      setImageUrl(url);
+    } else {
+      console.error('Görsel oluşturma başarısız:', response.statusText);
+      // Hata mesajı gösterin
+    }
+  };
+
+  const styles = {
+    container: {
+      flex: 1,
+      padding: 50,
+      top:100,
+      backgroundColor: '#f5f5f5',
+    },
+    heading: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      color: '#333',
+    },
+    input: {
+      borderWidth: 1,
+      borderColor: '#ccc',
+      padding: 10,
+      borderRadius: 5,
+      fontSize: 16,
+    },
+    button: {
+      backgroundColor: '#000',
+      color: '#fff',
+      borderRadius: 5,
+      padding: 10,
+      marginTop: 10,
+    },
+    image: {
+      width: 300,
+      height: 300,
+      borderRadius: 20,
+      marginTop: 10,
+    },
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Metni giriniz:</Text>
+      <TextInput
+        value={userInput}
+        onChangeText={handleInputChange}
+        style={styles.input}
+      />
+      <Button title="Görsel Oluştur" onPress={generateImage} style={styles.button} />
+      {imageUrl && (
+        <View>
+          <Text style={styles.heading}>Oluşturulan Görsel:</Text>
+          <Image source={{ uri: imageUrl }} style={styles.image} />
+        </View>
+      )}
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
